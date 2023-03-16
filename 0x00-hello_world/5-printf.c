ca514d51542194dbd5fa8992809cc6c7c71addbb
mohamed <mohamedghalib157@gmail.com>
2023-03-16 15:08:57 -0700

    pc

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - prints the phrase wanted
+ * Return: 0 means that the program will execute successfully
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
