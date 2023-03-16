57c36199698bbdf7d277a622aacf24b48f883d9f
mohamed <mohamedghalib157@gmail.com>
2023-03-16 15:54:34 -0700

    pc

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
-int main(void){
+/**
+ * main - to print the phrase wanted
+int main(void)
+{
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-		return 1;}
+		return (1);
+}
