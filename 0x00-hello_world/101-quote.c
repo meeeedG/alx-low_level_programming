7dec4caf4f3d9401f9c759228f47adb1581df843
mohamed <mohamedghalib157@gmail.com>
2023-03-16 19:20:23 -0700

    pc

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - to print the phrase wanted
  * Return: 1 means that there is some error while executing the program
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 		return (1);
 }
