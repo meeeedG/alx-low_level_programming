d3b23150514755bfd70bd4229204bf0795a140a5
mohamed <mohamedghalib157@gmail.com>
2023-03-16 23:08:50 -0700

    ch

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,13 +1,20 @@
-#include <stdlib.h>
-#include <time.h>
 #include<stdio.h>
-/* main - to give random number and excute and print
+/* main - excute and print
  * return: 0 code runs with succes
  */
 
 int main(void)
 {
-        int n;
+	char b;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
+	for(b = 'a'; b <= 'z'; b++)
+	{
+		putchar(b);
+	}
+	for(b = 'A'; b <= 'Z'; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return 0;
+}
