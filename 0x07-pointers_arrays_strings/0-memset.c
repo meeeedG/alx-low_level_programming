f4000340a19c28470d626e89cb74791dd9b51008
mohamed <mohamedghalib157@gmail.com>
2023-04-03 05:02:09 -0700

    pt

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ * *_memset : fills memory with a constant byte
+ * @s : is the memory area to be filled
+ * @b : the copied char
+ * @n : is number of times to copy b
+ * Return : is pointer to memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_N_
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
